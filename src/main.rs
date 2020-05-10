95534e3d198b4974e89bc2714cd5367cccf0ed9c
Tom Bolton <bolto720@googlemail.com>
2020-05-10 22:05:23 +0100

    Working FOV

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ mod map;
 pub use map::*;
 mod player;
 pub use player::*;
+mod rect;
+pub use rect::*;
+mod visibility_system;
+pub use visibility_system::VisibilitySystem;
 
 pub struct State {
     ecs: World,
@@ -16,6 +20,8 @@ pub struct State {
 
 impl State {
     fn run_systems(&mut self) {
+        let mut vis = VisibilitySystem{};
+        vis.run_now(&self.ecs);
         self.ecs.maintain();
     }
 }
@@ -27,8 +33,7 @@ impl GameState for State {
         player_input(self, ctx);
         self.run_systems();
 
-        let map = self.ecs.fetch::<Vec<TileType>>();
-        draw_map(&map, ctx);
+        draw_map(&self.ecs, ctx);
 
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
@@ -45,24 +50,28 @@ fn main() -> rltk::BError {
 
     let mut gs = State { ecs: World::new() };
 
-    //Add map
-    gs.ecs.insert(new_map());
-
     //Register components
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<Player>();
+    gs.ecs.register::<ViewShed>();
+
+    //Add map
+    let map: Map = Map::new_map_rooms_and_corridors();
+    let (player_x, player_y) = map.rooms[0].centre();
+    gs.ecs.insert(map);
 
     //Create player
     gs.ecs
         .create_entity()
-        .with(Position { x: 40, y: 25 })
+        .with(Position { x: player_x, y: player_y })
         .with(Renderable {
             glyph: rltk::to_cp437('@'),
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
         .with(Player {})
+        .with(ViewShed {visible_tiles: Vec::new(), range: 8, dirty: true})
         .build();
 
     rltk::main_loop(context, gs)
