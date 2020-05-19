0fe900172490498363db40ddafff2f8fe4f97bc8
Tom Bolton <bolto720@googlemail.com>
2020-05-20 00:00:47 +0100

    Added tooltips and message logs, as well as a proper spawner

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,9 @@ pub use damage_system::DamageSystem;
 mod melee_combat_system;
 pub use melee_combat_system::MeleeCombatSystem;
 mod gui;
+pub use gamelog::GameLog;
+mod gamelog;
+mod spawner;
 
 #[derive(PartialEq, Copy, Clone)]
 pub enum RunState {
@@ -109,10 +112,12 @@ impl GameState for State {
 fn main() -> rltk::BError {
     use rltk::RltkBuilder;
     //let context = RltkBuilder::simple80x50().with_title("Logue").build()?;
-    let context = match RltkBuilder::simple(MAP_WIDTH as i32, MAP_HEIGHT as i32) {
+    let mut context = match RltkBuilder::simple(MAP_WIDTH as i32, MAP_HEIGHT as i32) {
         Ok(c) => c.with_title("Logue").build()?,
         Err(e) => return Err(e),
     };
+
+    //context.with_post_scanlines(true);
 
     let mut gs = State {
         ecs: World::new(),
@@ -129,91 +134,27 @@ fn main() -> rltk::BError {
     gs.ecs.register::<CombatStats>();
     gs.ecs.register::<SufferDamage>();
     gs.ecs.register::<WantsToMelee>();
-
+    
     //Add map
     let map: Map = Map::new_map_rooms_and_corridors();
     let (player_x, player_y) = map.rooms[0].centre();
 
+    //Create player
+    let player_entity = spawner::player(&mut gs.ecs, player_x, player_y);
+
     //Create enemies
-    let mut rng = rltk::RandomNumberGenerator::new();
-    for (i, room) in map.rooms.iter().skip(1).enumerate() {
-        let (x, y) = room.centre();
-        let glyph: rltk::FontCharType;
-        let name: String;
-        let roll = rng.roll_dice(1, 2);
-
-        match roll {
-            1 => {
-                glyph = rltk::to_cp437('g');
-                name = "Goblin".to_string();
-            }
-            _ => {
-                glyph = rltk::to_cp437('o');
-                name = "Orc".to_string();
-            }
-        }
-
-        gs.ecs
-            .create_entity()
-            .with(Position { x, y })
-            .with(Renderable {
-                glyph: glyph,
-                fg: RGB::named(rltk::RED),
-                bg: RGB::named(rltk::BLACK),
-            })
-            .with(Monster {})
-            .with(Name {
-                name: format!("{} #{}", &name, i),
-            })
-            .with(ViewShed {
-                visible_tiles: Vec::new(),
-                range: 8,
-                dirty: true,
-            })
-            .with(BlocksTile {})
-            .with(CombatStats {
-                max_hp: 16,
-                hp: 16,
-                defense: 1,
-                power: 4,
-            })
-            .build();
+    gs.ecs.insert(rltk::RandomNumberGenerator::new());
+    for room in map.rooms.iter().skip(1) {
+        spawner::spawn_room(&mut gs.ecs, room);
     }
 
-    //Create player
-    let player_entity = gs
-        .ecs
-        .create_entity()
-        .with(Position {
-            x: player_x,
-            y: player_y,
-        })
-        .with(Renderable {
-            glyph: rltk::to_cp437('@'),
-            fg: RGB::named(rltk::YELLOW),
-            bg: RGB::named(rltk::BLACK),
-        })
-        .with(Player {})
-        .with(Name {
-            name: "Player".to_string(),
-        })
-        .with(ViewShed {
-            visible_tiles: Vec::new(),
-            range: 8,
-            dirty: true,
-        })
-        .with(CombatStats {
-            max_hp: 30,
-            hp: 30,
-            defense: 2,
-            power: 5,
-        })
-        .build();
 
     gs.ecs.insert(map);
     gs.ecs.insert(Point::new(player_x, player_y));
     gs.ecs.insert(player_entity);
     gs.ecs.insert(RunState::PreRun);
+    gs.ecs.insert(gamelog::GameLog{ entries: vec!["Welcome to Logue".to_string()]});
+ 
 
     rltk::main_loop(context, gs)
 }
